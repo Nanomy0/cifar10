7fd9f374d7383969960ed4bb060428caee6f4131
Nanomy0 <2285986465@qq.com>
2024-03-10 15:13:58 +0800

    init

---
+++ predict.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import torchvision
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
